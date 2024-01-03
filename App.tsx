6c74ed49383806fb83a8a160b3f53a80d9e666c7
Subhodip Roy <subhodiproy281@gmail.com>
2024-01-03 19:22:08 +0530

    avatar added at drawer tab

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/RootNavigator';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootNavigator />
+      <PaperProvider>
+        <RootNavigator />
+      </PaperProvider>
     </NavigationContainer>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,10 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: ["nativewind/babel", 'react-native-reanimated/plugin'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
