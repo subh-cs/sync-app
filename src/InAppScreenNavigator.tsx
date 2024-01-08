de9ec0aa19602b656f1790a87fbead749c6d059d
Subhodip Roy <subhodiproy281@gmail.com>
2024-01-08 23:12:58 +0530

    added new nested navigation strategy

---
+++ src/InAppScreenNavigator.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DrawerNavigator from './DrawerNavigator';
+import Profile from './screens/Profile';
+import VideoPlayer from './screens/VideoPlayer';
+import VideoGallery from './screens/VideoGallery';
+import { useUser } from '@clerk/clerk-expo';
+import { IJob } from './RootNavigator';
+import { supabase } from './utils/supabase';
+
+const InAppScreenStack = createNativeStackNavigator();
+
+const InAppScreenNavigator = () => {
+
+    const user = useUser();
+    const [allJobs, setAllJobs] = React.useState<IJob[]>();
+
+    async function getAllJobs() {
+        const userEmailAddress = user.user?.emailAddresses[0].emailAddress;
+        const resFromDb = await supabase
+            .from('sync-job')
+            .select('*')
+            .eq('user_email', userEmailAddress);
+        console.log("allJobsFromDB", resFromDb.data);
+        const data = resFromDb.data?.reverse() as IJob[];
+        setAllJobs(data);
+    }
+
+    // add latest job to top of allJobs
+    function addJobToAllJobs(job: IJob) {
+        setAllJobs((prevAllJobs) => {
+            return [job, ...prevAllJobs!];
+        })
+    }
+
+    useEffect(() => {
+        if (user.isLoaded === true && user.isSignedIn === true) {
+            getAllJobs();
+        }
+    }, [])
+
+    return (
+        <InAppScreenStack.Navigator screenOptions={{ headerShown: false }}>
+            <InAppScreenStack.Screen name="DrawerNavigator" children={() => <DrawerNavigator allJobs={allJobs} addJobToAllJobs={addJobToAllJobs} />} />
+            <InAppScreenStack.Screen name="Profile" component={Profile} />
+            <InAppScreenStack.Screen name="VideoGallery" children={() => <VideoGallery allJobs={allJobs} />} />
+            <InAppScreenStack.Screen name="VideoPlayer" component={VideoPlayer} />
+        </InAppScreenStack.Navigator>
+    )
+}
+
+export default InAppScreenNavigator
--- src/RootNavigator.tsx
+++ src/RootNavigator.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
-import DrawerNavigator from './DrawerNavigator';
-import Profile from './screens/Profile';
-import VideoPlayer from './screens/VideoPlayer';
 import { SignedIn, SignedOut } from "@clerk/clerk-expo";
-import VideoGallery from './screens/VideoGallery';
-import { useUser } from '@clerk/clerk-expo';
-import { supabase } from './utils/supabase';
+import InAppScreenNavigator from './InAppScreenNavigator';
 
 const RootStack = createNativeStackNavigator();
 
@@ -20,43 +15,12 @@ export interface IJob {
 
 
 const RootNavigator = () => {
-    const user = useUser();
-    const [allJobs, setAllJobs] = React.useState<IJob[]>();
-    const userEmailAddress = user.user?.emailAddresses[0].emailAddress;
-
-    async function getAllJobs() {
-        const resFromDb = await supabase
-            .from('sync-job')
-            .select('*')
-            .eq('user_email', userEmailAddress);
-        console.log("allJobsFromDB", resFromDb.data);
-        const data = resFromDb.data?.reverse() as IJob[];
-        setAllJobs(data);
-    }
-
-    // add latest job to top of allJobs
-    function addJobToAllJobs(job: IJob) {
-        setAllJobs((prevAllJobs) => {
-            return [job, ...prevAllJobs!];
-        })
-    }
-
-
-    useEffect(() => {
-        console.log("RootNavigator.tsx", user.user?.emailAddresses[0].emailAddress)
-        if (user.isLoaded) {
-            getAllJobs();
-        }
-    }, [user.isLoaded, user.isSignedIn])
 
     return (
         <>
             <SignedIn>
                 <RootStack.Navigator screenOptions={{ headerShown: false }}>
-                    <RootStack.Screen name="DrawerNavigator" children={() => <DrawerNavigator allJobs={allJobs} addJobToAllJobs={addJobToAllJobs} />} />
-                    <RootStack.Screen name="Profile" component={Profile} />
-                    <RootStack.Screen name="VideoGallery" children={() => <VideoGallery allJobs={allJobs} />} />
-                    <RootStack.Screen name="VideoPlayer" component={VideoPlayer} />
+                    <RootStack.Screen name="InAppScreenNavigator" component={InAppScreenNavigator} />
                 </RootStack.Navigator>
             </SignedIn>
             <SignedOut>
