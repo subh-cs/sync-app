45509d776b06ff7a1bda56b07689f820d59009f7
Subhodip Roy <subhodiproy281@gmail.com>
2024-01-10 14:57:39 +0530

    added textinput at login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,54 +1,38 @@
 import React from "react";
-import * as WebBrowser from "expo-web-browser";
-import { SafeAreaView, View, Image, Text, TouchableOpacity } from "react-native";
-import { useOAuth } from "@clerk/clerk-expo";
-import { useWarmUpBrowser } from "../hooks/warmBrowser";
-import { AntDesign } from '@expo/vector-icons';
+import { SafeAreaView, View, Image, Text, TouchableOpacity, TextInput, ScrollView, Alert } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import env from "../../utils/env";
 
-WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
-  // Warm up the android browser to improve UX
-  // https://docs.expo.dev/guides/authentication/#improving-user-experience
-  useWarmUpBrowser();
-
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
-
-  const onPress = React.useCallback(async () => {
-    try {
-      const { createdSessionId, signIn, signUp, setActive } =
-        await startOAuthFlow();
-
-      if (createdSessionId) {
-        setActive ? setActive({ session: createdSessionId }) : null;
-      } else {
-        // Use signIn or signUp for next steps such as MFA
-      }
-    } catch (err) {
-      console.error("OAuth error", err);
+    const showAllLoadedEnv = () => {
+        Alert.alert("Loaded env", JSON.stringify(env));
     }
-  }, []);
+    
+    return (
+        <SafeAreaView className="h-full w-full px-4 bg-black">
+            <ScrollView className="h-full w-full flex px-4 bg-black" contentContainerStyle={{ justifyContent: "center", alignItems: "center", width: "100%" }}>
+                <View className="pt-16">
+                    <Image source={{ uri: "https://app.synclabs.so/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsync_icon_white.850b8eaa.png&w=3840&q=75" }} className="h-64 w-48"></Image>
+                    <View className="flex flex-row justify-start items-center gap-1 pt-4 mt-4">
+                        <Text className="text-white font-thin text-xl">Welcome to</Text>
+                        <Text className="text-white font-bold text-xl">sync labs.</Text>
+                    </View>
+                </View>
 
-  return (
-    <SafeAreaView className="flex justify-center items-center h-full bg-black w-full px-4">
-
-      <Image source={{ uri: "https://app.synclabs.so/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsync_icon_white.850b8eaa.png&w=3840&q=75" }} className="h-64 w-48"></Image>
-      <View className="flex flex-row justify-start items-center gap-1 pt-4 mt-4">
-        <Text className="text-white font-thin text-xl">Welcome to</Text>
-        <Text className="text-white font-bold text-xl">sync labs.</Text>
-      </View>
-
-      <TouchableOpacity
-        onPress={onPress}
-        className="p-4 bg-white rounded-md flex flex-row justify-center items-center w-full mt-32"
-      >
-        <AntDesign name="google" size={24} color="black" />
-        <Text className=" text-base pl-4">Continue with Google</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="light" />
-    </SafeAreaView>
-  );
+                <View className="w-full flex justify-center items-center pb-4 pt-8 gap-2">
+                    <TextInput className="border border-white w-full rounded-lg p-2 text-white" placeholder="Email" placeholderTextColor="white" textContentType="emailAddress" />
+                    <TextInput className="border border-white w-full rounded-lg p-2 text-white" placeholder="Password" placeholderTextColor="white" textContentType="password" secureTextEntry />
+                </View>
+                <TouchableOpacity
+                    // onPress={onPress}
+                    className="p-4 bg-white rounded-md flex flex-row justify-center items-center w-full"
+                >
+                    <Text className=" text-base pl-2 font-semibold">Continue</Text>
+                </TouchableOpacity>
+            </ScrollView>
+            <StatusBar style="light" />
+        </SafeAreaView>
+    );
 }
 export default Login;
